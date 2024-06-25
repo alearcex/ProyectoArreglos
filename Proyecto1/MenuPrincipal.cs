ead16a7dbf27266618b95fc11575e888f7cb84ba
aless <aless@GOLLODEUDA>
2024-06-24 19:07:28 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto1/MenuPrincipal.cs
@@ -0,0 +1,307 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgra.Proceso
+{
+    public class MenuPrincipal
+    {
+        public MenuPrincipal()
+        {
+        }
+        public void Menu()  
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("********************************************************");
+                Console.WriteLine("**            SISTEMA COMUNAL DE RECICLAJE            **");
+                Console.WriteLine("********************************************************");
+                Console.WriteLine("Seleccione una opción: ");
+                Console.WriteLine("1- Inicializar Vectores");
+                Console.WriteLine("2- Realizar Pagos");
+                Console.WriteLine("3- Consultar Pagos");
+                Console.WriteLine("4- Modificar Pagos");
+                Console.WriteLine("5- Eliminar Pagos");
+                Console.WriteLine("6- Submenú de reportes");
+                Console.WriteLine("7- Salir");
+                Console.WriteLine("Seleccione una opción...");
+                
+                var opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        //InicializarVectores();
+                        break;
+                    case "2":
+                        //RealizarPago();
+                        break;
+                    case "3":
+                        //ConsultarPago();
+                        break;
+                    case "4":
+                        //ModificarPago();
+                    case "5":
+                        //EliminarPago();
+                        break;
+                    case "6":
+                        MenuReportes.Menu();
+                        break;                    
+                    case "7":
+                        Console.WriteLine("Saliendo...");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Opción no válida, por favor inténtelo de nuevo.");
+                        Console.ResetColor();
+                        Thread.Sleep(3000); // Espera 3 segundos antes de continuar
+                        Console.Clear();
+                        break;
+
+                }
+            }
+        }
+
+        //#region "Métodos para las opciones del menú"
+        //public void InsertarRegistro()
+        //{
+        //    while (true)
+        //    {
+        //        Console.Clear();
+        //        Console.WriteLine("******************************************************");
+        //        Console.WriteLine("**         Registro de entrega de Reciclaje         **");
+        //        Console.WriteLine("******************************************************");
+        //        //Se crea el nuevo  para agregarlo a la lista
+        //        Registro registro = new Registro();
+
+        //        //Se llenan los datos utilizando los métodos de ingreso
+        //        registro.IdRegistro = Registros.Count + 1; //simula el identity
+        //        registro.Usuario = InsertarUsuario();
+        //        registro.IdMaterial = InsertarMaterial(Materiales);       
+        //        //Se obtienen los puntos que da ese material por kilo depositado
+        //        int puntos = Materiales.Where(x => x.IdMaterial == registro.IdMaterial).Select(x => x.PuntosXKg).First();
+        //        registro.CantidadKg = InsertarPeso();
+        //        registro.Fecha = DateTime.Now;
+        //        registro.Puntos = puntos * registro.CantidadKg;
+        //        Console.WriteLine("Puntos acumulados por depósito: {0}", registro.Puntos);
+
+        //        Registros.Add(registro);
+
+        //        //Se consulta si se quiere seguir en esta opción
+        //        Console.WriteLine("------------------------------------------------------");
+        //        Console.WriteLine("¿Desea ingresar información de otro registro? (S/N): ");
+        //        string continuar = Console.ReadLine();
+        //        if (continuar.ToUpper() != "S")
+        //        {
+        //            break;
+        //        }
+
+        //    }
+        //}
+        //public void ModificarRegistro()
+        //{
+        //    while (true)
+        //    {
+        //        Console.Clear();
+        //        Console.WriteLine("******************************************************");
+        //        Console.WriteLine("**              Modificar  Registro                 **");
+        //        Console.WriteLine("******************************************************");
+        //        var id = MenuReportes.InsertarID();
+
+
+        //        //Se valida si hay un registro con ese id
+        //        if (Existe(id))
+        //        {
+        //            //Obtiene la información del registro
+        //            Registro registroModificar = Registros.Where(r => r.IdRegistro == id).First();
+
+        //            //Vuelven a pedir los datos del registro para que sean modificados
+        //            MenuPrincipal menu = new MenuPrincipal();
+        //            registroModificar.IdMaterial = InsertarMaterial(menu.Materiales);
+        //            registroModificar.CantidadKg = InsertarPeso();
+        //            registroModificar.Fecha = DateTime.Now;
+        //            var material = menu.Materiales.Where(m => m.IdMaterial == registroModificar.IdMaterial).First();
+        //            registroModificar.Puntos = registroModificar.CantidadKg * material.PuntosXKg;
+
+        //            Console.BackgroundColor = ConsoleColor.Green;
+        //            Console.WriteLine("Cambios realizados con éxito.");//Se confirma que todo salió bien
+        //            Console.ResetColor();
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("Estudiante no encontrado.");
+        //        }
+
+        //        //Consulta si desea iniciar de nuevo en esta opción
+        //        Console.WriteLine("------------------------------------------------------");
+        //        Console.WriteLine("¿Desea realizar otra consulta para modificar? (S/N): ");
+        //        string continuar = Console.ReadLine();
+        //        if (continuar.ToUpper() != "S")
+        //        {
+        //            break;
+        //        }
+
+        //    }
+        //}
+        //public void EliminarRegistro()
+        //{
+        //    while (true)
+        //    {
+        //        Console.Clear();
+        //        Console.WriteLine("******************************************************");
+        //        Console.WriteLine("**                Eliminar  Registros               **");
+        //        Console.WriteLine("******************************************************");
+        //        var id = MenuReportes.InsertarID();
+
+        //        //Se valida si existe registro con esa cédula
+        //        if (Existe(id))
+        //        {
+        //            //Se elimina el registro con esa cédula
+        //            Registros.RemoveAll(registro => registro.IdRegistro == id);
+        //            Console.BackgroundColor = ConsoleColor.Green;
+        //            Console.WriteLine("El registro se eliminó correctamente");
+        //            Console.ResetColor();
+
+        //        }
+        //        else
+        //        {
+        //            Console.BackgroundColor = ConsoleColor.Red;
+        //            Console.WriteLine("Registro no encontrado.");
+        //            Console.ResetColor();
+        //        }
+
+        //        Console.WriteLine("------------------------------------------------------");
+        //        Console.WriteLine("¿Desea realizar otra consulta para eliminar? (S/N): ");
+        //        string continuar = Console.ReadLine();
+        //        if (continuar.ToUpper() != "S")
+        //        {
+        //            break;
+        //        }
+        //    }
+
+        //}
+        //#endregion
+
+        //#region Metodos de insertar
+        //private static decimal InsertarPeso()
+        //{
+        //    decimal peso;
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            Console.WriteLine("-Ingrese el peso total de material depositado: ");
+        //            //Se cambian los puntos por comas
+        //            var valor = Console.ReadLine().Replace(",", ".");
+        //            peso = Convert.ToDecimal(valor, new CultureInfo("en-US"));
+
+        //            // se verifica si el peso está dentro del rango válido
+        //            if (peso < (decimal)0.000)
+        //            {
+        //                //Mensaje que tendrá la excepción
+        //                throw new Exception("El peso a ingresar debe der mayor a 0");
+        //            }
+
+        //            break;
+        //        }
+        //        catch (FormatException)
+        //        {
+        //            //Excepción en caso de no ser decimal
+        //            Console.WriteLine("Error: Debe ingresar un número (se aceptan decimales).");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            //Excepción en caso de no ser mayor a 0
+        //            Console.WriteLine("Error: " + ex.Message); 
+        //        }
+        //    }
+
+        //    //Se usa para redondear el número a 3 decimales
+        //    return Math.Round(peso, 3); 
+        //}
+        //private static int InsertarMaterial(List<Materiales> materiales)
+        //{
+        //    int id;
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            Console.WriteLine("-Seleccione el tipo de material:");
+        //            foreach (var m in materiales)
+        //            {
+        //                Console.WriteLine("{0}- {1}", m.IdMaterial, m.Descripcion);
+        //            }
+
+        //            id = int.Parse(Console.ReadLine());
+        //            // se verifica si el peso está dentro del rango válido
+        //            if (id < 0 || id > materiales.Count)
+        //            {
+        //                //Mensaje que tendrá la excepción
+        //                throw new Exception("El código de material ingrsado no existe");
+        //            }
+
+        //            break;
+        //        }
+        //        catch (FormatException)
+        //        {
+        //            //Excepción en caso de no ser decimal
+        //            Console.WriteLine("Error: Debe ingresar un número.");
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            //Excepción en caso de no ser mayor a 0
+        //            Console.WriteLine("Error: " + ex.Message); 
+        //        }
+        //    }
+
+        //    return id; 
+        //}
+        //public static string InsertarUsuario()
+        //{
+        //    string user;
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            Console.WriteLine("-Ingrese el nombre de usuario:");
+        //            user = Console.ReadLine();
+
+
+        //            // Validar que no sea nulo o solo espacios
+        //            if (string.IsNullOrWhiteSpace(user))
+        //            {
+        //                throw new Exception("El usuario no puede estar vacío ni consistir solo de espacios.");
+        //            }
+
+
+        //            // Validar que el usuario solo contenga letras o números
+        //            foreach (char caracter in user)
+        //            {
+        //                if (!char.IsLetterOrDigit(caracter) && caracter != ' ' && !char.IsWhiteSpace(caracter))
+        //                {
+        //                    throw new Exception("El usuario debe contener solo letras y números.");
+        //                }
+        //            }
+
+        //            break; // Salir del bucle si no hay excepciones
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine("Error: " + ex.Message);
+        //        }
+        //    }
+        //    return user;
+        //}
+
+        //#endregion
+
+
+    }
+}
---
+++ Proyecto1/MenuReportes.cs
@@ -0,0 +1,271 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgra.Proceso
+{
+    public class MenuReportes
+    {
+        public static void Menu(/*arreglos con data*/)
+        {
+            var seguir = true;
+            while(seguir)
+            {
+                Console.Clear();
+                Console.WriteLine("******************************************************");
+                Console.WriteLine("**           Reportes de Pagos Realizados           **");
+                Console.WriteLine("******************************************************");
+                Console.WriteLine("1- Ver todos los Pagos");
+                Console.WriteLine("2- Ver Pagos por tipo de Servicio");
+                Console.WriteLine("3- Ver Pagos por código de caja");
+                Console.WriteLine("4- Ver Dinero Comisionado por servicios");
+                Console.WriteLine("5- Regresar al Menú Principal");
+                Console.WriteLine("Seleccione una opción: ");
+                var opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        //ReporteTodos(arreglos con data);
+                        break;
+                    case "2":
+                        //ReportePorTipo(arreglos con data);
+                        break;
+                    case "3":
+                        //ReportePorCaja(arreglos con data);
+                        break;
+                    case "4":
+                        //ReporteDinero(registros);
+                        break;                    
+                    case "5":
+                        seguir = false;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Opción no válida, por favor inténtelo de nuevo.");
+                        Console.ResetColor();
+                        Thread.Sleep(3000); // Espera 3 segundos antes de continuar
+                        Console.Clear();
+                        break;
+                }
+            }
+
+        }
+
+        #region Métodos guía
+        //private static void BusquedaXUsuario(List<Registro> registros)
+        //{
+        //    var lista = new List<Registro>();
+
+        //    Console.Clear();
+        //    Console.WriteLine("********************************************************");
+        //    Console.WriteLine("**   Consulta de depósitos de reciclaje por Usuario   **");
+        //    Console.WriteLine("********************************************************");
+        //    //Se usa el método para obtener el usuario
+        //    string user = MenuPrincipal.InsertarUsuario();
+        //    //Se obtiene una lista de los registros que contengan ese usuario
+        //    lista = registros.Where(x => x.Usuario.Equals(user)).OrderBy(x => x.IdRegistro).ToList();
+
+        //    if (lista.Count() > 0)
+        //    {
+        //        Console.WriteLine($"{"ID",-5}{"Material",-15}{"Cantidad",-10}{"Fecha",-20}{"Pts Ganados",-11}");
+        //        Console.WriteLine("===================================================================");
+
+        //        foreach (var r in lista)
+        //        {
+        //            MenuPrincipal menu = new MenuPrincipal();
+        //            //Se obtiene la descripción del material para usarla en la tabla de resultados
+        //            string material = menu.Materiales.Where(x => x.IdMaterial == r.IdMaterial).Select(x => x.Descripcion).First();
+        //            //Se formatea la fecha
+        //            string fecha = r.Fecha.ToString("dd/MM/yyyy HH:mm:ss");
+        //            Console.WriteLine($"{r.IdRegistro,-5}{material,-15}{r.CantidadKg,-10}{fecha,-20}{r.Puntos,-11}");
+        //        }
+        //        Console.WriteLine("===================================================================");
+        //        Console.WriteLine("Total de depoísitos: {0}", lista.Count.ToString());
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No existen registros para este usuario");
+        //    }
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("            << Pulse cualquier tecla para volver >>            ");
+        //    Console.ReadKey();
+        //}
+        //private static void BusquedaXId(List<Registro> registros)
+        //{
+
+        //    var lista = new List<Registro>();
+
+        //    Console.Clear();
+        //    Console.WriteLine("********************************************************");
+        //    Console.WriteLine("**     Consulta de depósitos de reciclaje por ID      **");
+        //    Console.WriteLine("********************************************************");
+        //    //Se usa el método para obtener el ID
+        //    int id = InsertarID();
+        //    //Se obtiene el registro con ese id
+        //    var registro = registros.Find(x => x.IdRegistro == id);
+        //    MenuPrincipal menu = new MenuPrincipal();
+        //    //Se obtiene la descripción del material para usarla en los resultados
+        //    string material = menu.Materiales.Where(x => x.IdMaterial == registro.IdMaterial).Select(x => x.Descripcion).First();
+
+
+        //    if (registro != null)
+        //    {
+        //        Console.WriteLine("Usuario: {0}", registro.Usuario);
+        //        Console.WriteLine("Material: {0}", material);
+        //        Console.WriteLine("Cantidad (kg): {0}", registro.CantidadKg);
+        //        Console.WriteLine("Fecha: {0}", registro.Fecha.ToString("dd/MM/yyyy HH:mm:ss"));
+        //        Console.WriteLine("Puntos ganados: {0}", registro.Puntos);
+
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No existen registro para este ID");
+        //    }
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("            << Pulse cualquier tecla para volver >>            ");
+        //    Console.ReadKey();
+        //}
+        //private static void BusquedaXMaterial(List<Registro> registros)
+        //{
+        //    var lista = new List<Registro>();
+        //    //Se obtiene la descripción del material para usarla en los resultados
+        //    MenuPrincipal menu = new MenuPrincipal();
+        //    var materiales = menu.Materiales;
+
+        //    Console.Clear();
+        //    Console.WriteLine("********************************************************");
+        //    Console.WriteLine("**   Consulta de depósitos de reciclaje por Material  **");
+        //    Console.WriteLine("********************************************************");
+        //    Console.WriteLine("-Seleccione el tipo de material:");
+        //    foreach (var m in materiales)
+        //    {
+        //        Console.WriteLine("{0}- {1}", m.IdMaterial, m.Descripcion);
+        //    }
+
+        //    int id = InsertarID();
+
+        //    //Se obtiene una lista de los registros que contengan ese material
+        //    lista = registros.Where(x => x.IdMaterial.Equals(id)).OrderBy(x => x.IdRegistro).ToList();
+
+        //    if (lista.Count() > 0)
+        //    {
+        //        Console.WriteLine($"{"ID",-5}{"Material",-15}{"Cantidad",-10}{"Fecha",-20}{"Pts Ganados",-11}");
+        //        Console.WriteLine("===================================================================");
+
+        //        foreach (var r in lista)
+        //        {
+
+        //            //Se formatea la fecha
+        //            string material = menu.Materiales.Where(x => x.IdMaterial == r.IdMaterial).Select(x => x.Descripcion).First();
+        //            string fecha = r.Fecha.ToString("dd/MM/yyyy HH:mm:ss");
+        //            Console.WriteLine($"{r.IdRegistro,-5}{material,-15}{r.CantidadKg,-10}{fecha,-20}{r.Puntos,-11}");
+        //        }
+        //        Console.WriteLine("===================================================================");
+        //        Console.WriteLine("Total de depósitos: {0}", lista.Count.ToString());
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No existen registros para este material");
+        //    }
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("            << Pulse cualquier tecla para volver >>            ");
+        //    Console.ReadKey();
+        //}
+        //private static void InformeGeneral(List<Registro> registros)
+        //{
+
+        //    var vidrios = new List<Registro>();
+        //    var papeles = new List<Registro>();
+        //    var aluminios = new List<Registro>();
+        //    var plasticos = new List<Registro>();
+        //    var tetrapaks = new List<Registro>();
+
+        //    vidrios = registros.Where(x => x.IdMaterial == 1).ToList();
+        //    papeles = registros.Where(x => x.IdMaterial == 2).ToList();
+        //    aluminios = registros.Where(x => x.IdMaterial == 3).ToList();
+        //    plasticos = registros.Where(x => x.IdMaterial == 4).ToList();
+        //    tetrapaks = registros.Where(x => x.IdMaterial == 5).ToList();
+
+        //    //Porcentaje que representa cada material del total recolectado
+        //    var totalRegistros = registros.Count;
+        //    var porcVidrio = vidrios.Count > 0 ? (vidrios.Count * 100) / totalRegistros : 0;
+        //    var porcPapeles = papeles.Count > 0 ? (papeles.Count * 100) / totalRegistros : 0;
+        //    var porcAluminio = aluminios.Count > 0 ? (aluminios.Count * 100) / totalRegistros : 0;
+        //    var porcPlasticos = plasticos.Count > 0 ? (plasticos.Count * 100) / totalRegistros : 0;
+        //    var porcTetrapaks = tetrapaks.Count > 0 ? (tetrapaks.Count * 100) / totalRegistros : 0;
+
+        //    //Peso total
+        //    var kgVidrio = vidrios.Sum(v => v.CantidadKg);
+        //    var kgPapeles = papeles.Sum(v => v.CantidadKg);
+        //    var kgAluminio = aluminios.Sum(v => v.CantidadKg);
+        //    var kgPlasticos = plasticos.Sum(v => v.CantidadKg);
+        //    var kgTetrapaks = tetrapaks.Sum(v => v.CantidadKg);
+
+
+        //    Console.Clear();
+        //    Console.WriteLine("******************************************************************");
+        //    Console.WriteLine("****                    INFORME    GENERAL                    ****");
+        //    Console.WriteLine("******************************************************************");
+        //    Console.WriteLine("Vidrios:");
+        //    Console.WriteLine("-Peso total: {0}", kgVidrio);
+        //    Console.WriteLine("-Cantidad de registros: {0}", vidrios.Count);
+        //    Console.WriteLine("-Porcentaje de ingreso: {0}%", porcVidrio);
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("\nPapeles y Cartón:");
+        //    Console.WriteLine("-Peso total: {0}", kgPapeles);
+        //    Console.WriteLine("-Cantidad de registros: {0}", papeles.Count);
+        //    Console.WriteLine("-Porcentaje de ingreso: {0}%", porcPapeles);
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("\nAluminio:");
+        //    Console.WriteLine("-Peso total: {0}", kgAluminio);
+        //    Console.WriteLine("-Cantidad de registros: {0}", aluminios.Count);
+        //    Console.WriteLine("-Porcentaje de ingreso: {0}%", porcAluminio);
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("\nPlásticos:");
+        //    Console.WriteLine("-Peso total: {0}", kgPlasticos);
+        //    Console.WriteLine("-Cantidad de registros: {0}", plasticos.Count);
+        //    Console.WriteLine("-Porcentaje de ingreso: {0}%", porcPlasticos);
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("\nTetraPak:");
+        //    Console.WriteLine("-Peso total: {0}", kgTetrapaks);
+        //    Console.WriteLine("-Cantidad de registros: {0}", tetrapaks.Count);
+        //    Console.WriteLine("-Porcentaje de ingreso: {0}%", porcTetrapaks);
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("Totales:");
+        //    Console.WriteLine("-Peso total: {0}", kgTetrapaks + kgPlasticos + kgPapeles + kgVidrio + kgAluminio);
+        //    Console.WriteLine("-Cantidad de registros: {0}", totalRegistros);
+        //    Console.WriteLine("===================================================================");
+        //    Console.WriteLine("            << Pulse cualquier tecla para volver >>            ");
+        //    Console.ReadKey();
+        //}
+        //public static int InsertarID()
+        //{
+        //    int id;
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            Console.WriteLine("-Ingrese ID que desea buscar: ");
+        //            id = int.Parse(Console.ReadLine());
+        //            break;
+        //        }
+        //        catch
+        //        {
+        //            Console.BackgroundColor = ConsoleColor.Red;
+        //            Console.WriteLine("Error: Solo se permite el ingreso de números");
+        //            Console.ResetColor();
+        //        }
+
+        //    }
+        //    return id;
+            
+        //}
+        #endregion
+
+    }
+}
